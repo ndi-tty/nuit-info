333f44f63f3cd47c52b08c057d762c9db295c60b
Charley <charley.geoffroy@etu.umontpellier.fr>
2023-12-08 00:12:35 +0100

    feat(eco-mode): add eco mode toggle button
    Signed-off-by: Charley <charley.geoffroy@etu.umontpellier.fr>

--- web/src/layouts/header.tsx
+++ web/src/layouts/header.tsx
@@ -1,22 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./header.css";
 
 export interface HeaderProps {
-  year: number;
   message: string;
 }
+enum TimeManagement {
+  START_TIME = 1980,
+  END_TIME = 2024,
+  DURATION_IN_MINUTES = 1,
+}
+export const Header: React.FC<HeaderProps> = ({ message }) => {
+  const [year, setYear] = useState(TimeManagement.START_TIME);
 
-export const Header: React.FC<HeaderProps> = ({ year, message }) => {
-  const [count, setCount] = useState(0);
+  useEffect(() => {
+    const endTime = TimeManagement.END_TIME;
+    const startTime = year;
+    const totalTimeInYears = endTime - startTime;
+    const durationInMinutes = 1;
+    const totalIntervals = totalTimeInYears * 12;
 
+    let currentInterval = 0;
+
+    const interval = setInterval(() => {
+      const currentYear =
+        startTime +
+        Math.floor((currentInterval / totalIntervals) * totalTimeInYears);
+      setYear(currentYear);
+
+      currentInterval += 1;
+
+      if (currentInterval >= totalIntervals) {
+        clearInterval(interval);
+      }
+    }, (durationInMinutes * 60 * 1000) / totalIntervals);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
-    <header className="app-header">
-      <h1 className="logo">{message}</h1>
-      <div className="right-part-header">
-        <button onClick={() => setCount(count + 1)}>Eco-Mode</button>
-        <button onClick={() => setCount(count - 1)}>Thème</button>
-        <p className="year">{year}</p>
-      </div>
-    </header>
+    <>
+      <header className="app-header">
+        <h1 className="logo">{message}</h1>
+        <div className="right-part-header">
+          <input className="tgl tgl-skewed" id="cb3" type="checkbox" />
+          <label
+            onClick={() => console.log("Eco mode activé")}
+            className="tgl-btn"
+            data-tg-off="OFF"
+            data-tg-on="ON"
+            htmlFor="cb3"
+          ></label>
+
+          <button onClick={() => console.log("theme changed")}>Thème</button>
+          <p className="year">{year}</p>
+        </div>
+      </header>
+    </>
   );
 };
