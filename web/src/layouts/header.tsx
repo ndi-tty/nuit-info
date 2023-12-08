1f88f07103d0dd826212abf5b5334ef3790b7bc9
Charley <charley.geoffroy@etu.umontpellier.fr>
2023-12-08 06:12:41 +0100

    feat: le saviez-vous ?
    Signed-off-by: Charley <charley.geoffroy@etu.umontpellier.fr>

--- web/src/layouts/header.tsx
+++ web/src/layouts/header.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../store/store";
 import { setCounter } from "../store/slices/yearCounter";
@@ -15,9 +15,31 @@ enum TimeManagement {
   END_TIME = 2024,
   DURATION_IN_MINUTES = 1,
 }
+export const anecdotesEcolo = [
+  "En 2020, l'horloge de l'apocalypse a été créée par des scientifiques de l'université de Chicago. Cette horloge représente la probabilité d'une catastrophe planétaire. Plus l'horloge est proche de minuit, plus la probabilité d'une catastrophe est élevée.",
+  "2005 : L'ouragan Katrina frappe la côte sud-est des États-Unis, provoquant d'importantes inondations à La Nouvelle-Orléans. Les experts suggèrent que l'intensité accrue des ouragans est liée au réchauffement des océans.",
+  "2013 : Typhon Haiyan, l'un des plus puissants typhons jamais enregistrés, frappe les Philippines, entraînant des milliers de décès et des dégâts considérables. Les scientifiques soulignent la possible corrélation entre l'intensité des typhons et le changement climatique.",
+  "2017 : Les incendies de forêt dévastateurs en Californie détruisent des milliers de maisons et entraînent des pertes humaines importantes. Les conditions météorologiques extrêmes et les périodes de sécheresse prolongées sont liées au changement climatique.",
+  "2018 : Les inondations meurtrières dans l'État du Kerala, en Inde, sont amplifiées par des précipitations extrêmes, attribuées en partie aux changements climatiques.",
+  "2019-2020 : Les feux de brousse en Australie, qui ont brûlé des millions d'acres, sont associés à des températures record et à des conditions météorologiques extrêmes amplifiées par le changement climatique.",
+  "2020 : L'ouragan Laura devient l'un des ouragans les plus puissants à toucher terre en Louisiane, soulignant la tendance à des ouragans plus forts dans certaines régions.",
+  "2021 : Les inondations dévastatrices en Allemagne et en Belgique, avec des précipitations exceptionnelles, mettent en évidence le lien potentiel entre le changement climatique et l'augmentation des événements météorologiques extrêmes.",
+];
 
 export const Header: React.FC<HeaderProps> = ({ message }) => {
   const dispatch = useDispatch();
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+  const popupRef = useRef(null);
+  const [importantInformation, setimportantInformation] = useState(
+    "En 1947, l'horloge de l'apocalypse a été créée par des scientifiques de l'université de Chicago. Cette horloge représente la probabilité d'une catastrophe planétaire. Plus l'horloge est proche de minuit, plus la probabilité d'une catastrophe est élevée. En 2020, l'horloge a été avancée à 100 secondes de minuit, ce qui est le plus proche de minuit que l'horloge ait jamais été. En 2021, l'horloge a été avancée à 100 secondes de minuit, ce qui est le plus proche de minuit que l'horloge ait jamais été. En 2021, l'horloge a été avancée à 100 secondes de minuit, ce qui est le plus proche de minuit que l'horloge ait jamais été. En 2021, l'horloge a été avancée à 100 secondes de minuit, ce qui est le plus proche de minuit que l'horloge ait jamais été. En 2021, l'horloge a été avancée à 100 secondes de minuit, ce qui est le plus proche de minuit que l'horloge ait jamais été. En 2021, l'horloge a été avancée à 100 secondes de minuit, ce qui est le plus proche de minuit que l'horloge ait jamais été. En 2021, l'horloge a été avancée à 100 secondes de minuit, ce qui est le plus proche de minuit que l'horloge ait jamais été."
+  );
+
+  const handleYearClick = () => {
+    setimportantInformation(
+      anecdotesEcolo[Math.floor(Math.random() * anecdotesEcolo.length)]
+    );
+    setIsPopupVisible(!isPopupVisible);
+  };
 
   const yearCount = useSelector(
     (state: RootState) => state.yearCounter.yearsCounter
@@ -72,8 +94,28 @@ export const Header: React.FC<HeaderProps> = ({ message }) => {
               <p>Mode éco</p>
             </div>
           ) : null}
-          <p className="year">{yearCount}</p>
+          <p
+            className="year"
+            onClick={handleYearClick}
+            style={{
+              cursor: "pointer",
+            }}
+          >
+            {yearCount}
+          </p>
         </div>
+        {isPopupVisible && (
+          <div className="popup" ref={popupRef}>
+            <p
+              onClick={() => setIsPopupVisible(false)}
+              style={{ cursor: "pointer" }}
+            >
+              X
+            </p>
+            <h2 className="helper-title">Le saviez-vous ?</h2>
+            <p>{importantInformation}</p>
+          </div>
+        )}
       </header>
     </>
   );
